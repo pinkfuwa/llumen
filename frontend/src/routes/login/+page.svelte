bce3377652702dcb1109ca7c5cd3d6a1181e5684
Eason(G Ray) <30045503+Eason0729@users.noreply.github.com>
2025-07-25 11:48:17 +0800

    Login Page

---
+++ frontend/src/routes/login/+page.svelte
@@ -0,0 +1,23 @@
+<div class="flex h-screen flex-col items-center justify-center">
+	<div class="mb-2 text-3xl">Sign in to llumen</div>
+	<div class="text-md mb-4 font-extralight">
+		simple LLM chat frontend with great out-of-box experience
+	</div>
+	<div class="min-w-lg items-center rounded-lg p-6">
+		<div class="flex flex-col items-center justify-center text-xl">
+			<div class="mb-2 flex w-full justify-between">
+				<div class="min-w-[130px]">Username:</div>
+				<input type="text" placeholder="Username" class="mb-2 grow border-b border-outline pb-2" />
+			</div>
+			<div class="mb-6 flex w-full justify-between">
+				<div class="min-w-[130px]">Password:</div>
+				<input
+					type="password"
+					placeholder="Password"
+					class="mb-2 grow border-b border-outline pb-2"
+				/>
+			</div>
+			<button class="rounded-full border border-outline px-8 py-2 hover:bg-hover">Sign in</button>
+		</div>
+	</div>
+</div>
