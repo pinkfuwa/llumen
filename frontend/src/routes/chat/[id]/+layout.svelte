c3ac2fcccb73329cec43eba801e35d834c174f1f
Eason(G Ray) <30045503+Eason0729@users.noreply.github.com>
2025-08-20 01:56:46 +0800

    chore: various layout improvement

--- frontend/src/routes/chat/[id]/+layout.svelte
+++
@@ -1,8 +0,0 @@
-<script lang="ts">
-	let { children, params } = $props();
-</script>
-
-<svelte:head>
-	<title>Chatroom {params.id}</title>
-</svelte:head>
-{@render children()}
--- frontend/src/routes/chat/new/+layout.svelte
+++
@@ -1,9 +0,0 @@
-<script>
-	let { children } = $props();
-	import { _ } from 'svelte-i18n';
-</script>
-
-<svelte:head>
-	<title>{$_('chat.title')}</title>
-</svelte:head>
-{@render children()}
