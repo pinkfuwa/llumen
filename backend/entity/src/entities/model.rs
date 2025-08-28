c9e60fc59eba6929aeb7aa0a82accee43a9fb161
Eason(G Ray) <30045503+Eason0729@users.noreply.github.com>
2025-08-28 19:47:32 +0800

    feat: Add model list and json def to Model model

--- backend/entity/src/entities/model.rs
+++ backend/entity/src/entities/model.rs
@@ -7,7 +7,7 @@ use sea_orm::entity::prelude::*;
 pub struct Model {
     #[sea_orm(primary_key)]
     pub id: i32,
-    pub config: String,
+    pub config: crate::patch::ModelConfig,
 }
 
 #[derive(Copy, Clone, Debug, EnumIter, DeriveRelation)]
