bfaa8708d8c55f1676743006657ef00126e3942f
Eason(G Ray) <30045503+Eason0729@users.noreply.github.com>
2025-08-28 23:09:14 +0800

    feat: delete user

--- frontend/src/lib/components/setting/UserGrid.svelte
+++ frontend/src/lib/components/setting/UserGrid.svelte
@@ -1,28 +1,33 @@
 <script lang="ts">
-	import { useUser, useUsers } from '$lib/api/user';
+	import { DeleteUser, useUser, useUsers } from '$lib/api/user';
 	import { Trash } from '@lucide/svelte';
 	import { _ } from 'svelte-i18n';
 
-	const { isLoading, data } = useUsers();
+	const { mutate: deleteUser } = DeleteUser();
+	const { data } = useUsers();
 
-	const { data: userData } = useUser();
+	const { isLoading, data: userData } = useUser();
 </script>
 
 {#if $isLoading}
 	<div class="mb-4 flex items-center justify-center p-6 text-lg">Loading users...</div>
 {:else if $data != undefined}
 	<ul class="grid max-h-[50vh] grid-cols-1 gap-2 overflow-y-auto pb-2 text-lg lg:grid-cols-2">
-		{#each $data as user}
+		{#each $data.list as user}
 			<li
 				class="flex min-h-[50px] shrink-0 items-center justify-between rounded-lg border border-outline py-1 pr-2 pl-4"
 			>
-				{user.username}
-				<!-- TODO: expose delete button if not current user -->
-				<!-- {#if user.id != $userData?.user_id}
-					<button onclick={() => {}}>
+				{user.name}
+				{#if !isLoading && user.id != $userData?.user_id}
+					<button
+						onclick={() =>
+							deleteUser({
+								user_id: user.id
+							})}
+					>
 						<Trash class="h-10 w-10 rounded-lg p-2 hover:bg-hover" />
 					</button>
-				{/if} -->
+				{/if}
 			</li>
 		{/each}
 	</ul>
