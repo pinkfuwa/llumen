d02d353d1cbc3eee03284782f1f298cfbdb1cbcf
Eason(G Ray) <30045503+Eason0729@users.noreply.github.com>
2025-09-02 15:00:33 +0800

    chore: Rewrite message's style to support mult-part message

---
+++ frontend/src/lib/components/message/buttons/ResponseBox.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	import { FolderSearch } from '@lucide/svelte';
+	import { _ } from 'svelte-i18n';
+
+	let { children } = $props();
+</script>
+
+<div class="group w-full space-y-2 px-10 py-2 wrap-break-word lg:px-20 2xl:px-36">
+	<div class="mb-2 border-b border-outline pb-2 select-none">
+		<FolderSearch class="mr-2 inline-block" />
+		{$_('chat.assistant.response')}
+	</div>
+
+	{@render children()}
+</div>
