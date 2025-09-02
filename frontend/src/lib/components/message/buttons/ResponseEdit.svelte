7e71588541c28c0630bc36255eeee83e821f28fd
Eason(G Ray) <30045503+Eason0729@users.noreply.github.com>
2025-09-02 15:00:33 +0800

    chore: Rewrite message's style to support mult-part message

---
+++ frontend/src/lib/components/message/buttons/ResponseEdit.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+	import { copy } from '$lib/copy';
+	import { CircleDollarSign, ClipboardCopy } from '@lucide/svelte';
+	import { _ } from 'svelte-i18n';
+
+	let { content = '', token = 0, cost = 0.0 } = $props();
+</script>
+
+<div class="flex justify-end space-x-1 opacity-0 group-hover:opacity-100">
+	<div class="group/usage relative flex space-x-1">
+		<CircleDollarSign class="h-10 w-10 rounded-lg p-2 group-hover/usage:bg-hover hover:bg-hover" />
+
+		<div
+			class="absolute top-0 right-13 hidden h-10 w-sm items-center justify-end group-hover/usage:flex"
+		>
+			<div class="rounded-md bg-background p-2 select-none">
+				{token} token/${cost.toFixed(4)}
+			</div>
+		</div>
+	</div>
+	<button onclick={() => copy(content)}>
+		<ClipboardCopy class="h-10 w-10 rounded-lg p-2 hover:bg-hover" />
+	</button>
+</div>
---
+++ frontend/src/lib/components/message/buttons/ResponseError.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	import Warning from '$lib/components/setting/Warning.svelte';
+	import { CloudAlert } from '@lucide/svelte';
+	import { _ } from 'svelte-i18n';
+</script>
+
+<Warning>
+	<CloudAlert class="mr-2 inline-block h-7 w-7" />
+	{$_('chat.error.no_output')}
+</Warning>
