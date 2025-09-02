d9441668bf6b795f3e8b3a42322b8953b09b54f9
Eason(G Ray) <30045503+Eason0729@users.noreply.github.com>
2025-09-02 15:01:37 +0800

    fix: rename AssitantBox to ResponseBox

--- frontend/src/lib/components/message/MessageStream.svelte
+++ frontend/src/lib/components/message/MessageStream.svelte
@@ -2,13 +2,13 @@
 	import type { TokensList } from 'marked';
 	import Parser from '$lib/markdown/Parser.svelte';
 	import { _ } from 'svelte-i18n';
-	import AssitantBox from './buttons/AssitantBox.svelte';
+	import ResponseBox from './buttons/ResponseBox.svelte';
 
 	const { list }: { list: TokensList[] } = $props();
 </script>
 
-<AssitantBox>
+<ResponseBox>
 	{#each list as tokens, i}
 		<Parser {tokens} monochrome />
 	{/each}
-</AssitantBox>
+</ResponseBox>
