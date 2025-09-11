4a692385eebbdb0c55e7f9916c5c14f2a7b82df0
kaiyohugo <41114603+KAIYOHUGO@users.noreply.github.com>
2025-09-12 01:22:39 +0800

    feat: Add tool call support & wttr example

--- backend/src/tools/mod.rs
+++ backend/src/tools/mod.rs
@@ -5,3 +5,12 @@ mod tool;
 pub use set::*;
 pub use store::*;
 pub use tool::*;
+
+use crate::tool_set;
+
+pub mod wttr;
+
+pub const NORMAL: ToolSet = tool_set![wttr::Wttr];
+pub const SEARCH: ToolSet = tool_set![wttr::Wttr];
+pub const AGENT: ToolSet = tool_set![wttr::Wttr];
+pub const RESEARCH: ToolSet = tool_set![wttr::Wttr];
--- backend/src/tools/tool.rs
+++ backend/src/tools/tool.rs
@@ -1,30 +1,22 @@
 use anyhow::{Context, Result};
-use axum::response::{IntoResponse, Response};
-use futures_util::{FutureExt, future::LocalBoxFuture};
+use futures_util::{FutureExt, future::BoxFuture};
 use schemars::JsonSchema;
 use serde::{Serialize, de::DeserializeOwned};
 use serde_json::Value;
 
-pub trait Tool: Serialize + DeserializeOwned + Default + 'static {
-    /// for LLM
-    type Input: JsonSchema + DeserializeOwned;
-    type Output: IntoResponse;
-
-    // State for the UI
-    type State: Serialize;
+pub trait Tool: Serialize + DeserializeOwned + Default + Send + 'static {
+    type Input: JsonSchema + DeserializeOwned + Send;
+    type Output: Serialize;
 
     const NAME: &str;
     const DESCRIPTION: &str;
     const PROMPT: &str;
 
-    fn call(
-        &mut self,
-        input: Self::Input,
-    ) -> impl Future<Output = Result<(Self::Output, Self::State)>>;
+    fn call(&mut self, input: Self::Input) -> impl Future<Output = Result<Self::Output>> + Send;
 }
 
-pub trait UntypedTool {
-    fn call<'a>(&'a mut self, input: Value) -> LocalBoxFuture<'a, Result<(Response, Value)>>;
+pub trait UntypedTool: Send {
+    fn call<'a>(&'a mut self, input: Value) -> BoxFuture<'a, Result<Value>>;
     fn se(&self) -> Result<String>;
 }
 
@@ -32,17 +24,13 @@ impl<T> UntypedTool for T
 where
     T: Tool,
 {
-    fn call<'a>(&'a mut self, input: Value) -> LocalBoxFuture<'a, Result<(Response, Value)>> {
+    fn call<'a>(&'a mut self, input: Value) -> BoxFuture<'a, Result<Value>> {
         async {
-            Tool::call(self, serde_json::from_value(input)?)
+            Ok(Tool::call(self, serde_json::from_value(input)?)
                 .await
-                .map(|(resp, state)| {
-                    serde_json::to_value(state)
-                        .context("Cannot se the value")
-                        .map(|state| (resp.into_response(), state))
-                })?
+                .map(|output| serde_json::to_value(output))??)
         }
-        .boxed_local()
+        .boxed()
     }
 
     fn se(&self) -> Result<String> {
