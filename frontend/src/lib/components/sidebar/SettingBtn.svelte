74ff9e974b59f18014529c05353ff7c17c6c44f0
kaiyohugo <41114603+KAIYOHUGO@users.noreply.github.com>
2025-09-20 23:35:28 +0800

    chore: style tweak

--- frontend/src/lib/components/sidebar/SettingBtn.svelte
+++ frontend/src/lib/components/sidebar/SettingBtn.svelte
@@ -4,10 +4,12 @@
 	import Button from '$lib/ui/Button.svelte';
 	import { Dialog } from 'bits-ui';
 
+	let { value = $bindable() } = $props();
 	let { data, isLoading } = useUser();
 </script>
 
 <Dialog.Trigger
+	onclick={() => (value = 'account')}
 	class="flex w-full items-center justify-between rounded-lg border border-outline px-3 py-2 text-center text-sm font-medium text-text duration-150 hover:bg-primary hover:text-text-hover focus:ring-4 focus:ring-outline focus:outline-none  "
 >
 	<span class="font-medium"
