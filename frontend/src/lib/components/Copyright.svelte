69e614ab63d73bc69f6595f81cf9b61a3daeb08f
kaiyohugo <41114603+KAIYOHUGO@users.noreply.github.com>
2025-09-20 19:58:35 +0800

    fix: copyright style

--- frontend/src/lib/components/Copyright.svelte
+++ frontend/src/lib/components/Copyright.svelte
@@ -11,11 +11,11 @@
 		<a
 			href="https://github.com/pinkfuwa/llumen"
 			target="_blank"
-			class="rounded-md px-[6px] py-0.5 hover:bg-hover hover:text-dark">llumen</a
+			class="rounded-md px-2 py-0.5 hover:bg-primary hover:text-text-hover duration-150">llumen</a
 		>Made by<a
 			href="https://github.com/pinkfuwa"
 			target="_blank"
-			class="rounded-md px-[6px] py-0.5 hover:bg-hover hover:text-dark">pinkfuwa</a
+			class="rounded-md px-2 py-0.5 hover:bg-primary hover:text-text-hover duration-150">pinkfuwa</a
 		>&COPY; {new Date().getFullYear()}
 	</footer>
 </div>
