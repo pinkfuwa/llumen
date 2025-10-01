bcea036112349dbdd2658f7f385dfc8ff83c447d
Eason(G Ray) <30045503+Eason0729@users.noreply.github.com>
2025-10-01 11:39:13 +0800

    fix: markdown table rendering

--- frontend/src/lib/components/markdown/TableCell.svelte
+++ frontend/src/lib/components/markdown/TableCell.svelte
@@ -3,9 +3,13 @@
 </script>
 
 {#if header}
-	<th {align} class="px-6 py-3 text-center text-xs font-medium tracking-wider uppercase"
+	<th {align} class="px-6 py-3 text-center text-xs tracking-wider uppercase"
 		>{@render children?.()}</th
 	>
 {:else}
-	<td {align} class="px-6 py-4 font-semibold whitespace-nowrap">{@render children?.()}</td>
+	<td {align} class="px-6 py-4 font-semibold">
+		<div>
+			{@render children?.()}
+		</div>
+	</td>
 {/if}
--- frontend/src/lib/components/message/buttons/ResponseBox.svelte
+++ frontend/src/lib/components/message/buttons/ResponseBox.svelte
@@ -5,7 +5,7 @@
 	let { children } = $props();
 </script>
 
-<div class="group w-full space-y-2 px-10 py-2 wrap-break-word lg:px-20 2xl:px-36">
+<div class="group w-full space-y-2 px-[5vw] py-2 wrap-break-word lg:px-20 2xl:px-36">
 	<div class="mb-2 border-b border-outline pb-2 select-none">
 		<FolderSearch class="mr-2 inline-block" />
 		{$_('chat.assistant.response')}
