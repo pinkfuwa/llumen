b6b6a4adc1e12d70582f0827a626dab08f5a9a5e
Eason(G Ray) <30045503+Eason0729@users.noreply.github.com>
2025-10-03 00:25:43 +0800

    docs: add comment to internal code

--- backend/src/middlewares/compression.rs
+++ backend/src/middlewares/compression.rs
@@ -7,6 +7,7 @@ impl<S> tower::Layer<S> for ZstdCompressionLayer {
     type Service = tower_http::compression::Compression<S>;
 
     fn layer(&self, inner: S) -> Self::Service {
+        // only zstd, to make better server binary size
         TowerCompressionLayer::new()
             .no_deflate()
             .no_br()
