dae4509c19280f07de6c65ec7159ab531ecb6644
Eason(G Ray) <30045503+Eason0729@users.noreply.github.com>
2025-10-17 00:02:41 +0800

    feat: better scroll bar using damping

--- frontend/src/lib/ui/Scroll.svelte
+++ frontend/src/lib/ui/Scroll.svelte
@@ -5,41 +5,53 @@
 	const props = $props<
 		HTMLAttributes<HTMLElement> & {
 			children: Component;
-			speed?: number;
-			friction?: number;
+			damping?: number;
 		}
 	>();
 
-	const { speed = 0.15, friction = 0.92, children } = props;
+	const { damping = 0.2, children } = props;
 
 	let scrollElement = $state<null | HTMLDivElement>(null);
 	let velocity = $state(0);
-	let staticFration = $state(0);
-	let isAnimating = $derived(Math.abs(velocity) > Math.max(1, staticFration));
+	let animationFrameId: number | null = null;
+	let isAnimating = $derived(animationFrameId != null);
+
+	const velocityThreshold = 3;
 
 	function onwheel(event: WheelEvent) {
+		if (Math.abs(event.deltaY / event.deltaX) < 1) return;
 		event.preventDefault();
 
-		const sameDirection = event.deltaY * velocity > 0;
-		const delta = event.deltaY;
-		const speedDelta = delta * speed + 1;
-
-		if (isAnimating && sameDirection) {
-			velocity *= 1.7;
-			if (Math.abs(velocity) < Math.abs(speedDelta)) velocity = speedDelta;
-		} else {
-			staticFration = Math.min(14, delta * 0.1);
-			velocity = speedDelta;
-			animate();
+		if (animationFrameId !== null) {
+			cancelAnimationFrame(animationFrameId);
+			animationFrameId = null;
 		}
+
+		const impulse = event.deltaY * (1 - damping);
+		velocity += impulse;
+
+		if (!isAnimating) animate();
 	}
 
 	function animate() {
-		if (!scrollElement || !isAnimating) velocity = 0;
-		else {
-			scrollElement.scrollTop += Math.round(velocity);
-			velocity *= friction;
-			requestAnimationFrame(animate);
+		if (!scrollElement) {
+			if (animationFrameId !== null) {
+				cancelAnimationFrame(animationFrameId);
+				animationFrameId = null;
+			}
+			return;
+		}
+
+		velocity *= 1 - damping;
+
+		scrollElement.scrollTop += Math.round(velocity);
+
+		if (Math.abs(velocity) > velocityThreshold) {
+			animationFrameId = requestAnimationFrame(animate);
+		} else {
+			isAnimating = false;
+			velocity = 0;
+			animationFrameId = null;
 		}
 	}
 </script>
