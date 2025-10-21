d15b7062b47e062a4c832221d340eb9b18924299
Eason(G Ray) <30045503+Eason0729@users.noreply.github.com>
2025-10-21 17:14:49 +0800

    feat: use last selected model

--- frontend/src/lib/store.ts
+++ frontend/src/lib/store.ts
@@ -19,7 +19,7 @@ export function localState<T>(
 	const tokenStore = writable(storedValue);
 
 	tokenStore.subscribe((value) => {
-		if (value) {
+		if (value !== undefined) {
 			localStorage.setItem(key, JSON.stringify(value));
 		} else {
 			localStorage.removeItem(key);
