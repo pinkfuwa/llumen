2bb2cea925cf1df761cd48cfa43b0c51add0fe37
KAIYOHUGO <41114603+KAIYOHUGO@users.noreply.github.com>
2025-10-25 14:27:18 +0800

    feat: UI tweak (#11)

--- frontend/src/lib/components/sidebar/SettingBtn.svelte
+++ frontend/src/lib/components/sidebar/SettingBtn.svelte
@@ -12,7 +12,8 @@
 
 <Dialog.Trigger
 	onclick={() => (value = 'account')}
-	class="flex w-full items-center justify-between rounded-lg border border-outline px-3 py-2 text-center text-sm font-medium text-text duration-150 hover:bg-primary hover:text-text-hover focus:ring-4 focus:ring-outline focus:outline-none  "
+	class="flex w-full cursor-pointer items-center justify-between rounded-lg border border-outline px-3 py-2 text-center
+	text-sm font-medium text-text duration-150 hover:bg-primary hover:text-text-hover focus:ring-4 focus:ring-outline focus:outline-none"
 >
 	<span class="font-medium"
 		>{#if $data == undefined}
