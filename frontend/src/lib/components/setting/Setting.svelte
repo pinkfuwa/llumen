8c0a64782235111516bda5115a8cf81297417322
Eason(G Ray) <30045503+Eason0729@users.noreply.github.com>
2025-10-27 10:05:13 +0800

    feat: setting size on tablet

--- frontend/src/lib/components/setting/Setting.svelte
+++ frontend/src/lib/components/setting/Setting.svelte
@@ -25,7 +25,7 @@
 			class="fixed inset-0 z-50 backdrop-blur-md fade-in-100 fade-out-0 data-[state=closed]:animate-out data-[state=open]:animate-in"
 		/>
 		<Dialog.Content
-			class="md:6/7 fixed inset-0 z-50 m-auto flex rounded-xl border border-outline bg-popup-bg p-3 font-mono text-text fade-in fade-out zoom-in zoom-out data-[state=closed]:animate-out data-[state=open]:animate-in md:w-5/7 lg:h-5/7 lg:w-3/5 xl:h-5/6"
+			class="fixed inset-0 z-50 m-2 flex max-w-4xl rounded-xl border border-outline bg-popup-bg p-3 font-mono text-text fade-in fade-out zoom-in zoom-out data-[state=closed]:animate-out data-[state=open]:animate-in md:m-auto md:h-[min(80vh,48rem)] md:w-full"
 		>
 			<Dialog.Close
 				class="focus-visible:ring-foreground focus-visible:ring-offset-background absolute top-5 right-5 rounded-md focus-visible:ring-2 focus-visible:ring-offset-2 focus-visible:outline-hidden active:scale-[0.98]"
