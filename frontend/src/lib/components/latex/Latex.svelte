20b336f36171c7f2fbf72b7a82249364192b7426
Eason(G Ray) <30045503+Eason0729@users.noreply.github.com>
2025-11-13 15:50:18 +0800

    fix: filickering on latex
    Signed-off-by: Eason(G Ray) <30045503+Eason0729@users.noreply.github.com>

--- frontend/src/lib/components/latex/Latex.svelte
+++ frontend/src/lib/components/latex/Latex.svelte
@@ -2,8 +2,12 @@
 	import { toHtml } from './latex';
 
 	let { text, displayMode = false } = $props<{ text: string; displayMode?: boolean }>();
+	let html = $state<string | null>(null);
+	$effect(() => {
+		toHtml(text, displayMode).then((res) => (html = res));
+	});
 </script>
 
-{#await toHtml(text, displayMode) then html}
+{#if html != null}
 	{@html html}
-{/await}
+{/if}
