30274571955f079994bc2e33ef874d3d6ef2858e
Eason(G Ray) <30045503+Eason0729@users.noreply.github.com>
2025-11-30 15:14:58 +0800

    fix: add Task node for markdown
    Signed-off-by: Eason(G Ray) <30045503+Eason0729@users.noreply.github.com>

---
+++ frontend/src/lib/components/markdown/Task.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	import { SquareCheck } from '@lucide/svelte';
+	import type { Snippet } from 'svelte';
+
+	let { children }: { children: Snippet } = $props();
+</script>
+
+<div class="flex flex-row items-center">
+	<SquareCheck class="mr-2 min-h-8 min-w-8 p-1" />
+	<span>
+		{@render children?.()}
+	</span>
+</div>
