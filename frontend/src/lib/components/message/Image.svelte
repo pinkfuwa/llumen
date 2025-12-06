6771b736aa89efa9c5be7d7a8274035f44ea3539
Eason(G Ray) <30045503+Eason0729@users.noreply.github.com>
2025-12-06 16:00:02 +0800

    chore: image style
    Signed-off-by: Eason(G Ray) <30045503+Eason0729@users.noreply.github.com>

--- frontend/src/lib/components/message/Image.svelte
+++ frontend/src/lib/components/message/Image.svelte
@@ -34,17 +34,17 @@
 	</div>
 {:else if src}
 	<div class="my-2 flex justify-center">
-		<div class="border-border group relative rounded-lg border">
+		<div class="group relative">
 			<img
 				{src}
 				alt="Generated"
-				class="h-auto max-h-[min(30rem,85vw,70vh)] max-w-full"
+				class="border-border group relative h-auto max-h-[min(30rem,85vw,70vh)] max-w-full rounded-lg border"
 				loading="lazy"
 			/>
 			<button
 				onclick={downloadImage}
 				aria-label="download image"
-				class="visible absolute top-2 right-2 rounded-lg p-2 duration-150 hover:bg-primary hover:text-text-hover md:invisible md:group-hover:visible"
+				class="visible absolute top-2 right-2 rounded-lg bg-secondary p-2 duration-150 hover:bg-primary hover:text-text-hover md:invisible md:group-hover:visible"
 			>
 				<Download class="h-5 w-5" />
 			</button>
@@ -53,9 +53,3 @@
 {:else}
 	<div class="border-border my-2 flex justify-center rounded-lg border p-4">Loading image...</div>
 {/if}
-
-<style>
-	img {
-		display: block;
-	}
-</style>
