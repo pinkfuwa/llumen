0821eb3ebf20c32105414c1787f0a75f3df56a7f
Eason(G Ray) <30045503+Eason0729@users.noreply.github.com>
2026-01-01 16:44:53 +0800

    fix: prevent gesture on scroll
    Signed-off-by: Eason(G Ray) <30045503+Eason0729@users.noreply.github.com>

--- frontend/src/lib/components/markdown/Table.svelte
+++ frontend/src/lib/components/markdown/Table.svelte
@@ -4,7 +4,7 @@
 	let { children }: { children: Snippet } = $props();
 </script>
 
-<div class="flex justify-center">
+<div class="flex touch-none justify-center">
 	<div class="relative my-1 overflow-x-auto rounded-md border border-outline">
 		<table
 			class="divide-y divide-gray-200 bg-component-bg whitespace-nowrap lg:whitespace-break-spaces"
