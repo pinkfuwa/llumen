c2a3acbc68d41d96e0cd192aea79fcb8a064a870
Eason0729 <30045503+Eason0729@users.noreply.github.com>
2026-02-28 22:50:07 +0800

    chore: hide token usage if it's zero

--- frontend/src/lib/components/message/ResponseEdit.svelte
+++ frontend/src/lib/components/message/ResponseEdit.svelte
@@ -4,21 +4,24 @@
 	import { _ } from 'svelte-i18n';
 
 	let { content = '', token = 0, cost = 0.0 } = $props();
+	let showUsage = $derived(token > 0);
 </script>
 
 <div class="flex justify-end space-x-1 duration-150 group-hover:visible md:invisible">
 	<div class="group/usage relative flex space-x-1">
-		<CircleDollarSign
-			class="h-10 w-10 rounded-lg p-2 duration-150  group-hover/usage:bg-primary group-hover/usage:text-text-hover"
-		/>
+		{#if showUsage}
+			<CircleDollarSign
+				class="h-10 w-10 rounded-lg p-2 duration-150  group-hover/usage:bg-primary group-hover/usage:text-text-hover"
+			/>
 
-		<div
-			class="absolute top-0 right-13 flex h-10 w-sm items-center justify-end duration-150 group-hover/usage:visible md:invisible"
-		>
-			<div class="rounded-md bg-secondary p-2 select-none">
-				{token} token/${cost.toFixed(4)}
+			<div
+				class="absolute top-0 right-13 flex h-10 w-sm items-center justify-end duration-150 group-hover/usage:visible md:invisible"
+			>
+				<div class="rounded-md bg-secondary p-2 select-none">
+					{token} token/${cost.toFixed(4)}
+				</div>
 			</div>
-		</div>
+		{/if}
 	</div>
 	<button onclick={() => copy(content)} aria-label="copy response">
 		<ClipboardCopy
