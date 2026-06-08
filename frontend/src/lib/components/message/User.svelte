ac77ceb6b39321c4f438fc5f677a7fa2bd1de831
邱繼叡 Eason CHIU <30045503+Eason0729@users.noreply.github.com>
2026-06-08 16:25:33 +0800

    chore: remove snap on user message

--- frontend/src/lib/components/message/User.svelte
+++ frontend/src/lib/components/message/User.svelte
@@ -76,7 +76,7 @@
 	}
 </script>
 
-<div class="group/files mt-4 w-full snap-start px-[5vw] lg:px-20 2xl:px-36" id={`${messageId}`}>
+<div class="group/files mt-4 w-full px-[5vw] lg:px-20 2xl:px-36" id={`${messageId}`}>
 	<div class="flex justify-end">
 		<div class={messageStyle} data-state={isEditing ? 'edit' : 'text'}>
 			{#if files.length != 0}
